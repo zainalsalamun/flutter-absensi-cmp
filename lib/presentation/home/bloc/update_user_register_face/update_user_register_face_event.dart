c8266667b1295da4deafc96f018b96351d9e3cb2
zainalsalamun <zainalrtf@gmail.com>
2026-03-18 22:58:26 +0700

    refactor: remove face detection and user registration features
    - Deleted UpdateUserRegisterFaceEvent and UpdateUserRegisterFaceState files as they are no longer needed.
    - Removed CameraViewAttendancePage and FaceDetectorCheckinPage, which handled camera functionalities and face detection for attendance.
    - Eliminated FaceDetectorPainter widget used for drawing detected faces on the camera preview.

--- lib/presentation/home/bloc/update_user_register_face/update_user_register_face_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'update_user_register_face_bloc.dart';
-
-@freezed
-class UpdateUserRegisterFaceEvent with _$UpdateUserRegisterFaceEvent {
-  const factory UpdateUserRegisterFaceEvent.started() = _Started;
-  const factory UpdateUserRegisterFaceEvent.updateProfileRegisterFace(
-      String embedding, XFile? image) = _UpdateProfileRegisterFace;
-}
